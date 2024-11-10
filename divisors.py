4191a87a09ca4c43ef29b2b739abf612dedfb258
unknown <ghwjd025@naver.com>
2024-11-10 23:01:10 +0900

    Add divisors.py

--- divisors.py
+++ divisors.py
@@ -1,8 +1,6 @@
 import sys
 number = int(sys.argv[1])
-
-# 1부터 number까지 나누어떨어지는 수를 찾음
-for i in range(1, number + 1):  # 1부터 number까지 반복
-    if number % i == 0:  # 나누어떨어지는 수일 때
+for i in range(1, number + 1):  # loop between 1 and number
+    if number % i == 0:  # check if remainder is 0
         print(i, end=" ")
-print()  # 출력 끝내기
+print()
